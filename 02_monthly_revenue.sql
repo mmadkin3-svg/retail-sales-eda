57c20fbbce5b6f6978089cc77dbdb996e70e32de
mmadkin3-svg <mmadkin3@gmail.com>
2026-04-17 20:14:49 -0500

    Delete 02_monthly_revenue.sql

--- 02_monthly_revenue.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  FORMAT_DATE('%b %Y', DATE(Date))  AS month_label,
-  EXTRACT(YEAR  FROM Date)            AS yr,
-  EXTRACT(MONTH FROM Date)            AS mo,
-  ROUND(SUM(Total_Amount), 2)         AS total_revenue,
-  COUNT(*)                            AS transaction_count
-FROM `circular-acumen-445903-j5.retail_eda.retail_sales`
-GROUP BY 1, 2, 3
-ORDER BY 2, 3
