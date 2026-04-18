0f9bc68c41b2ec3a9671b748f238d5bd97a95b18
mmadkin3-svg <mmadkin3@gmail.com>
2026-04-17 20:15:15 -0500

    Delete 05_gender_category.sql

--- 05_gender_category.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  Gender,
-  Product_Category,
-  COUNT(*)                         AS transactions,
-  ROUND(AVG(Total_Amount), 2)      AS avg_spend,
-  ROUND(SUM(Total_Amount), 2)      AS total_revenue
-FROM `circular-acumen-445903-j5.retail_eda.retail_sales`
-GROUP BY 1, 2
-ORDER BY 1, 2
