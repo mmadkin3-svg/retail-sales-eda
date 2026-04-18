41a89dce2cf9ac84e3fbf781b9804c658c8169fc
mmadkin3-svg <mmadkin3@gmail.com>
2026-04-17 19:32:02 -0500

    Update 01_quality_check.sql

--- sql/01_quality_check.sql
+++ sql/01_quality_check.sql
@@ -1,5 +1,12 @@
 -- Quality check: NULL validation
 SELECT
-  COUNTIF(`Transaction ID` IS NULL) AS null_txn,
-  COUNTIF(`Total Amount` IS NULL) AS null_total
+COUNTIF(Transaction_ID   IS NULL) AS null_txn_id,
+  COUNTIF(Date             IS NULL) AS null_date,
+  COUNTIF(Customer_ID      IS NULL) AS null_cust,
+  COUNTIF(Gender           IS NULL) AS null_gender,
+  COUNTIF(Age              IS NULL) AS null_age,
+  COUNTIF(Product_Category IS NULL) AS null_category,
+  COUNTIF(Quantity         IS NULL) AS null_qty,
+  COUNTIF(Price_per_Unit   IS NULL) AS null_price,
+  COUNTIF(Total_Amount     IS NULL) AS null_total
 FROM `your-project.retail_eda.retail_sales`
